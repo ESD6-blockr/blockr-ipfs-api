5fe2b58b70ffb2d8ff080f3c1ac212ca5fbdf634
Arnoud Bevers <arnoud@arnoudbevers.com>
2019-06-17 10:20:28 +0200

    [ADD] Full working and documented PoC of version control

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,9 @@
 const express = require("express"),
   router = express.Router(),
-  ipfsController = require("../api/controllers/ipfsController");
+  ipfsController = require("../api/controllers/ipfsController"),
+  ipnsController = require("../api/controllers/ipnsController");
 
 router.use("/ipfs", ipfsController);
+router.use("/ipns", ipnsController);
 
 module.exports = router;
